69c48cd4cbdade76d628b36b806aa3ec82ffddc3
Piyush Kumar <24145@iiitu.ac.in>
2025-08-15 21:26:14 +0530

    fixed

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,4 +1,4 @@
-const Item = require("../models/item");
+const Item = require("../models/Item");
 const checkAndMatchItem = async (newItem) => {
   try {
     const oppositeType = newItem.type === "lost" ? "found" : "lost";
